eb6cf8aee27abfefc2ba636a90dc204b41ac5d6d
CreativePauloo <pmwendwa42@gmail.com>
2025-04-17 01:40:59 +0300

    changes

--- budgeting_app_backend/build.sh
+++ budgeting_app_backend/build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
 # Install dependencies
-pip install -r requirements.txt
+poetry install --no-interaction --no-ansi
 
 # Collect static files
 python manage.py collectstatic --no-input
--- budgeting_app_backend/start.sh
+++ budgeting_app_backend/start.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
 # Run migrations
 python manage.py migrate
 
 # Start Gunicorn
-gunicorn budgeting_app_backend.wsgi:application \
+poetry run gunicorn budgeting_app_backend.wsgi:application \
   --bind 0.0.0.0:$PORT \
   --workers 4 \
-  --timeout 120 \
-  --log-level debug
+  --timeout 120
