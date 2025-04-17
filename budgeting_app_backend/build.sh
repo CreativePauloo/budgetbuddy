d22615e8abc39cd43b5dcfd8891c80595bb4e28d
CreativePauloo <pmwendwa42@gmail.com>
2025-04-17 14:32:34 +0300

    changes

--- budgeting_app_backend/build.sh
+++ budgeting_app_backend/build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-poetry install --no-interaction --no-ansi
-python manage.py collectstatic --no-input
-python manage.py migrate
+set -o errexit
+
+# Install Poetry if not present
+if ! command -v poetry &> /dev/null; then
+    pip install poetry
+fi
+
+# Configure Poetry to create virtualenvs in project
+poetry config virtualenvs.in-project true
+
+# Install dependencies
+poetry install --no-interaction --no-ansi --with dev
+
+# Collect static files
+poetry run python manage.py collectstatic --no-input
+
+# Apply database migrations
+poetry run python manage.py migrate
--- budgeting_app_backend/start.sh
+++ budgeting_app_backend/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-poetry run gunicorn budgeting_app_backend.wsgi:application \
-  --bind 0.0.0.0:$PORT \
-  --workers 4 \
-  --timeout 120
+set -o errexit
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Run migrations
+python manage.py migrate
+
+# Start Gunicorn
+exec gunicorn budgeting_app_backend.wsgi:application \
+    --bind 0.0.0.0:$PORT \
+    --workers 4 \
+    --timeout 120 \
+    --log-level debug
