3b883097799c62bc216578af81664994c6368a98
CreativePauloo <pmwendwa42@gmail.com>
2025-04-22 15:39:48 +0300

    changes

--- budgeting-app-frontend/src/App.js
+++ budgeting-app-frontend/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import ForgotPassword from './components/auth/ForgotPassword';
