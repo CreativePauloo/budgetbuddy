d6925c90db2ebe58bd2630501ba66d527560beee
CreativePauloo <pmwendwa42@gmail.com>
2025-04-29 10:17:45 +0300

    changes

--- budgeting_app_backend/api/urls.py
+++ budgeting_app_backend/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from .views import (
     DashboardDataView,
     UserView,
@@ -17,20 +17,21 @@ from .views import (
 )
 
 urlpatterns = [
-    path('api/register/', UserRegistrationView.as_view(), name='register'),
-    path('api/login/', CustomTokenObtainPairView.as_view(), name='login'),
-    path('api/user/', UserView.as_view(), name='user'),
-    path('api/transactions/', TransactionView.as_view(), name='transactions'),
-    path('api/forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
-    path('api/comments/', CommentView.as_view(), name='comments'),
-    path('api/dashboard/', DashboardDataView.as_view(), name='dashboard'),
-    path('api/notifications/', NotificationView.as_view(), name='notifications'),
-    path('api/transactions/monthly/', MonthlyTransactionsView.as_view(), name='monthly-transactions'),
-    path('api/transactions/<int:pk>/', TransactionView.as_view(), name='transaction-detail'),
-    path('api/transaction-categories/', TransactionCategoriesView.as_view(), name='transaction-categories'),
-    path('api/notifications/<int:pk>/', NotificationView.as_view(), name='notification-detail'),
-    path('api/budgets/', BudgetView.as_view(), name='budgets'),
-    path('api/reports/', ReportView.as_view(), name='reports'),
-    path('api/chatbot/', ChatbotView.as_view(), name='chatbot'),
-    path('api/categorize/', predict_category, name='predict_category'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('user/', UserView.as_view(), name='user'),
+    path('transactions/', TransactionView.as_view(), name='transactions'),
+    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
+    path('comments/', CommentView.as_view(), name='comments'),
+    path('dashboard/', DashboardDataView.as_view(), name='dashboard'),
+    path('notifications/', NotificationView.as_view(), name='notifications'),
+    path('transactions/monthly/', MonthlyTransactionsView.as_view(), name='monthly-transactions'),
+    path('transactions/<int:pk>/', TransactionView.as_view(), name='transaction-detail'),
+    path('transaction-categories/', TransactionCategoriesView.as_view(), name='transaction-categories'),
+    path('notifications/<int:pk>/', NotificationView.as_view(), name='notification-detail'),
+    path('budgets/', BudgetView.as_view(), name='budgets'),
+    path('reports/', ReportView.as_view(), name='reports'),
+    path('chatbot/', ChatbotView.as_view(), name='chatbot'),
+    path('categorize/', predict_category, name='predict_category'),
+    
 ]
